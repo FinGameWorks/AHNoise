9553e6377f05cdade763cc0eca0da4a9ec60cc18
App Work <Appwork@andrews-macbook-air.local>
2016-06-02 15:17:52 +0100

    Initial Commit

---
+++ AHNoiseExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AHNoiseExample
+//
+//  Created by App Work on 02/06/2016.
+//  Copyright © 2016 App Work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
